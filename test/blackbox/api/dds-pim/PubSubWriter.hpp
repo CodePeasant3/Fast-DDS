ce4a12a13defe02621c610ae8b9c120f249935cf
adriancampo <adriancampo@eprosima.com>
2024-01-18 11:15:08 +0100

    Refs #20160: Fixed deadlock, renamed discovery methods. Applied suggestions.
    Signed-off-by: adriancampo <adriancampo@eprosima.com>

--- test/blackbox/api/dds-pim/PubSubWriter.hpp
+++ test/blackbox/api/dds-pim/PubSubWriter.hpp
@@ -116,7 +116,7 @@ class PubSubWriter
 
 #endif // if HAVE_SECURITY
 
-        void on_subscriber_discovery(
+        void on_data_reader_discovery(
                 eprosima::fastdds::dds::DomainParticipant*,
                 eprosima::fastrtps::rtps::ReaderDiscoveryInfo&& info) override
         {
@@ -135,7 +135,7 @@ class PubSubWriter
             }
         }
 
-        void on_publisher_discovery(
+        void on_data_writer_discovery(
                 eprosima::fastdds::dds::DomainParticipant*,
                 eprosima::fastrtps::rtps::WriterDiscoveryInfo&& info) override
         {
